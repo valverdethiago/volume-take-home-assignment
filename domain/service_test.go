dc8179b8ed5103ce6c0f93a2b6f9786f6824e5d3
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-11-21 19:55:41 -0300

    Unit test refactoring

--- domain/service_test.go
+++ domain/service_test.go
@@ -6,51 +6,78 @@ import (
 	"testing"
 )
 
-func TestFlight(t *testing.T) {
-	t.Run("single flight", func(t *testing.T) {
-		paths := []*Flight{NewFlight("SFO", "EWR")}
-		result, err := NewFlightPathServiceImpl().CalculateFlightPath(&paths)
-		require.NotNil(t, result)
-		require.Nil(t, err)
-		require.Equalf(t, result, paths, "input and output params should be equal")
+type TestCase struct {
+	name      string
+	paths     *[]*Flight
+	assertion func(t *testing.T, input *[]*Flight, result *[]*Flight, err error)
+}
+
+func (testCase *TestCase) run(t *testing.T) {
+	target := NewFlightPathServiceImpl()
+	t.Run(testCase.name, func(t *testing.T) {
+		result, err := target.CalculateFlightPath(testCase.paths)
+		testCase.assertion(t, testCase.paths, result, err)
 	})
+}
+
+func TestSingleFlight(t *testing.T) {
+	singleFlight := TestCase{
+		name:  "single flight",
+		paths: &[]*Flight{NewFlight("SFO", "EWR")},
+		assertion: func(t *testing.T, input *[]*Flight, result *[]*Flight, err error) {
+			require.NotNil(t, result)
+			require.Nil(t, err)
+			require.Equalf(t, result, input, "input and output params should be equal")
+		},
+	}
+	singleFlight.run(t)
 }
 
 func TestEmptyFlight(t *testing.T) {
-	t.Run("empty flight", func(t *testing.T) {
-		result, err := NewFlightPathServiceImpl().CalculateFlightPath(nil)
-		require.NotNil(t, err)
-		require.Nil(t, result)
-		require.Truef(t, errors.Is(err, EmptyFlightPathMapError), "Wrong error message")
-	})
+	emptyFlightList := TestCase{
+		name:  "empty flight list",
+		paths: nil,
+		assertion: func(t *testing.T, input *[]*Flight, result *[]*Flight, err error) {
+			require.NotNil(t, err)
+			require.Nil(t, result)
+			require.Truef(t, errors.Is(err, EmptyFlightPathMapError), "Wrong error message")
+		},
+	}
+	emptyFlightList.run(t)
 }
 
 func TestDoublePathScenario(t *testing.T) {
-	t.Run("double path flight", func(t *testing.T) {
-		paths := []*Flight{
+	doublePathFlight := TestCase{
+		name: "double path flight",
+		paths: &[]*Flight{
 			NewFlight("ATL", "EWR"),
 			NewFlight("SFO", "ATL"),
-		}
-		result, err := NewFlightPathServiceImpl().CalculateFlightPath(&paths)
-		require.NotNil(t, result)
-		require.Nil(t, err)
-		require.Equalf(t, (*result)[0].Origin, "SFO", "First origin should be SFO")
-		require.Equalf(t, (*result)[1].Destination, "EWR", "Last destination should be EWR")
-	})
+		},
+		assertion: func(t *testing.T, input *[]*Flight, result *[]*Flight, err error) {
+			require.NotNil(t, result)
+			require.Nil(t, err)
+			require.Equalf(t, (*result)[0].Origin, "SFO", "First origin should be SFO")
+			require.Equalf(t, (*result)[1].Destination, "EWR", "Last destination should be EWR")
+		},
+	}
+	doublePathFlight.run(t)
 }
 
 func TestCompletePathScenario(t *testing.T) {
-	t.Run("complete path flight", func(t *testing.T) {
-		paths := []*Flight{
+	completeScenario := TestCase{
+		name: "complete path flight",
+		paths: &[]*Flight{
 			NewFlight("IND", "EWR"),
 			NewFlight("SFO", "ATL"),
 			NewFlight("GSO", "IND"),
 			NewFlight("ATL", "GSO"),
-		}
-		result, err := NewFlightPathServiceImpl().CalculateFlightPath(&paths)
-		require.NotNil(t, result)
-		require.Nil(t, err)
-		require.Equalf(t, (*result)[0].Origin, "SFO", "First origin should be SFO")
-		require.Equalf(t, (*result)[3].Destination, "EWR", "Last destination should be EWR")
-	})
+		},
+		assertion: func(t *testing.T, input *[]*Flight, result *[]*Flight, err error) {
+			require.NotNil(t, result)
+			require.Nil(t, err)
+			require.Equalf(t, (*result)[0].Origin, "SFO", "First origin should be SFO")
+			require.Equalf(t, (*result)[3].Destination, "EWR", "Last destination should be EWR")
+		},
+	}
+	completeScenario.run(t)
 }
