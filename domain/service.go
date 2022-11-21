a5b55ebf1d52b40ea8e44c73e9b5eab5b64320a3
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-11-21 19:56:06 -0300

    Handling null or blank parameters and remove unused method

--- domain/service.go
+++ domain/service.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	"errors"
-	"math"
 )
 
 var EmptyFlightPathMapError = errors.New("it's not possible to calculate with an empty map")
@@ -19,7 +18,7 @@ func NewFlightPathServiceImpl() FlightPathService {
 }
 
 func (f FlightPathServiceImpl) CalculateFlightPath(paths *[]*Flight) (orderedPaths *[]*Flight, err error) {
-	if len(*paths) == 0 {
+	if paths == nil || len(*paths) == 0 {
 		return orderedPaths, EmptyFlightPathMapError
 	}
 	if len(*paths) == 1 {
@@ -36,7 +35,7 @@ func (f FlightPathServiceImpl) CalculateFlightPath(paths *[]*Flight) (orderedPat
 	return &result, nil
 }
 
-func (f FlightPathServiceImpl) findNextOrFirstFlight(paths *[]*Flight, previous *Flight) int {
+func (f FlightPathServiceImpl) findNextOrFirstFlight(paths *[]*Flight, previous *Flight) (result int) {
 out:
 	for i, path := range *paths {
 		if previous == nil {
@@ -44,48 +43,19 @@ out:
 				if path.Origin == (*paths)[j].Destination {
 					continue out
 				}
-				if j == len(*paths)-1 {
-					return i
-				}
 			}
+			result = i
+			break out
 		} else if path.Origin == previous.Destination {
-			return i
+			result = i
+			break out
 		}
 	}
-	return math.MaxInt
+	return result
 }
 
 func getAndRemove(paths *[]*Flight, index int) (*Flight, *[]*Flight) {
 	flight := (*paths)[index]
 	result := append((*paths)[:index], (*paths)[index+1:]...)
 	return flight, &result
-}
-
-func (f FlightPathServiceImpl) findFirstFlight(paths []Flight) (flight Flight, index int) {
-out:
-	for i, path := range paths {
-		for j := i; j < len(paths); j++ {
-			if path.Origin == paths[j].Destination {
-				continue
-			}
-			if j == len(paths)-1 {
-				flight = paths[j]
-				index = j
-				break out
-			}
-		}
-	}
-	return flight, index
-}
-
-func (f FlightPathServiceImpl) findNextFlight(paths []Flight, previous *Flight) (flight Flight, index int) {
-out:
-	for i, path := range paths {
-		if path.Origin == previous.Destination {
-			flight = path
-			index = i
-			break out
-		}
-	}
-	return flight, index
 }
