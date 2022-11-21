2d5de20865fcdc6601f36d2ddab14f3146cb159b
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-11-21 19:18:41 -0300

    Testing complete scenario

--- domain/service.go
+++ domain/service.go
@@ -1,11 +1,14 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 var EmptyFlightPathMapError = errors.New("it's not possible to calculate with an empty map")
 
 type FlightPathService interface {
-	CalculateFlightPath(paths []Flight) (orderedPaths []Flight, err error)
+	CalculateFlightPath(paths *[]*Flight) (orderedPaths *[]*Flight, err error)
 }
 
 type FlightPathServiceImpl struct {
@@ -15,21 +18,47 @@ func NewFlightPathServiceImpl() FlightPathService {
 	return &FlightPathServiceImpl{}
 }
 
-func (f FlightPathServiceImpl) CalculateFlightPath(paths []Flight) (orderedPaths []Flight, err error) {
-	if len(paths) == 0 {
+func (f FlightPathServiceImpl) CalculateFlightPath(paths *[]*Flight) (orderedPaths *[]*Flight, err error) {
+	if len(*paths) == 0 {
 		return orderedPaths, EmptyFlightPathMapError
 	}
-	if len(paths) == 1 {
+	if len(*paths) == 1 {
 		return paths, nil
 	}
-	var result []Flight
+	var result []*Flight
 	var next *Flight
-	for hasElement := true; hasElement; hasElement = len(paths) > 0 {
-		next, index := f.findNextOrFirstFlight(paths, next)
-		result = append(result, *next)
-		paths = removeIndex(paths, index)
+	for hasElement := true; hasElement; hasElement = len(*paths) > 0 {
+		index := f.findNextOrFirstFlight(paths, next)
+		//*next = *((*paths)[index])
+		next, paths = getAndRemove(paths, index)
+		result = append(result, next)
 	}
-	return result, nil
+	return &result, nil
+}
+
+func (f FlightPathServiceImpl) findNextOrFirstFlight(paths *[]*Flight, previous *Flight) int {
+out:
+	for i, path := range *paths {
+		if previous == nil {
+			for j := i + 1; j < len(*paths); j++ {
+				if path.Origin == (*paths)[j].Destination {
+					continue out
+				}
+				if j == len(*paths)-1 {
+					return i
+				}
+			}
+		} else if path.Origin == previous.Destination {
+			return i
+		}
+	}
+	return math.MaxInt
+}
+
+func getAndRemove(paths *[]*Flight, index int) (*Flight, *[]*Flight) {
+	flight := (*paths)[index]
+	result := append((*paths)[:index], (*paths)[index+1:]...)
+	return flight, &result
 }
 
 func (f FlightPathServiceImpl) findFirstFlight(paths []Flight) (flight Flight, index int) {
@@ -59,31 +88,4 @@ out:
 		}
 	}
 	return flight, index
-}
-
-func (f FlightPathServiceImpl) findNextOrFirstFlight(paths []Flight, previous *Flight) (flight *Flight, index int) {
-out:
-	for i, path := range paths {
-		if previous == nil {
-			for j := i; j < len(paths); j++ {
-				if path.Origin == (paths)[j].Destination {
-					continue
-				}
-				if j == len(paths)-1 {
-					flight = &paths[j]
-					index = j
-					break out
-				}
-			}
-		} else if path.Origin == previous.Destination {
-			flight = &path
-			index = i
-			break out
-		}
-	}
-	return flight, index
-}
-
-func removeIndex(paths []Flight, index int) []Flight {
-	return append(paths[:index], paths[index+1:]...)
 }
--- domain/service_test.go
+++ domain/service_test.go
@@ -6,11 +6,10 @@ import (
 	"testing"
 )
 
-func TestSingleFlight(t *testing.T) {
+func TestFlight(t *testing.T) {
 	t.Run("single flight", func(t *testing.T) {
-		singlePath := NewFlight("SFO", "EWR")
-		paths := []Flight{*singlePath}
-		result, err := NewFlightPathServiceImpl().CalculateFlightPath(paths)
+		paths := []*Flight{NewFlight("SFO", "EWR")}
+		result, err := NewFlightPathServiceImpl().CalculateFlightPath(&paths)
 		require.NotNil(t, result)
 		require.Nil(t, err)
 		require.Equalf(t, result, paths, "input and output params should be equal")
@@ -28,14 +27,30 @@ func TestEmptyFlight(t *testing.T) {
 
 func TestDoublePathScenario(t *testing.T) {
 	t.Run("double path flight", func(t *testing.T) {
-		paths := []Flight{
-			*NewFlight("ATL", "EWR"),
-			*NewFlight("SFO", "ATL"),
+		paths := []*Flight{
+			NewFlight("ATL", "EWR"),
+			NewFlight("SFO", "ATL"),
 		}
-		result, err := NewFlightPathServiceImpl().CalculateFlightPath(paths)
+		result, err := NewFlightPathServiceImpl().CalculateFlightPath(&paths)
 		require.NotNil(t, result)
 		require.Nil(t, err)
-		require.Equalf(t, result[0].Origin, "SFO", "First origin should be SFO")
-		require.Equalf(t, result[1].Destination, "EWR", "Last destination should be EWR")
+		require.Equalf(t, (*result)[0].Origin, "SFO", "First origin should be SFO")
+		require.Equalf(t, (*result)[1].Destination, "EWR", "Last destination should be EWR")
+	})
+}
+
+func TestCompletePathScenario(t *testing.T) {
+	t.Run("complete path flight", func(t *testing.T) {
+		paths := []*Flight{
+			NewFlight("IND", "EWR"),
+			NewFlight("SFO", "ATL"),
+			NewFlight("GSO", "IND"),
+			NewFlight("ATL", "GSO"),
+		}
+		result, err := NewFlightPathServiceImpl().CalculateFlightPath(&paths)
+		require.NotNil(t, result)
+		require.Nil(t, err)
+		require.Equalf(t, (*result)[0].Origin, "SFO", "First origin should be SFO")
+		require.Equalf(t, (*result)[3].Destination, "EWR", "Last destination should be EWR")
 	})
 }
